1419bfcfef058c6184509baf648554a345cef756
Vikram Subbaiah <vykrum@hotmail.com>
2020-04-12 09:26:41 +0530

    Add Bolero to Soluion

--- SpacePlanning/Clusters.fs
+++ SpacePlanning/Clusters.fs
@@ -1,22 +1,27 @@
-﻿namespace Clusters
-module HexShapes =
-    open System
+﻿
+namespace Clusters
+
+module Hexel =
 
     // Core and six adjacent cells
-    let adj (x : float, y: float) =
+    let adj (x : int, y: int) =
         // x : x coordinate
         // y : y coordinate
 
-        let hxx = [ 0.0; 0.0; 0.93; 0.93; 0.0; -0.93; -0.93 ]
-                |> List.map (fun i -> Math.Round(x + i,3))
+        let hxx = [ 0; -1; -2; -1; 1; 2; 1 ] |> List.map (fun i -> x + i)
+                
+        //let hxx = [ 0; 0.0; 0.93; 0.93; 0.0; -0.93; -0.93 ]
+        //        |> List.map (fun i -> Math.Round(x + i,3))
 
-        let hxy = [ 0.0; 1.074; 0.537; -0.537; -1.074; -0.537; 0.537 ]
-                |> List.map (fun i -> Math.Round(y + i, 3))
+        let hxy = [ 0; -2; 0; 2; 2; 0; -2 ] |> List.map (fun i -> y + i)
+                
+        //let hxy = [ 0; 1.074; 0.537; -0.537; -1.074; -0.537; 0.537 ]
+        //        |> List.map (fun i -> Math.Round(y + i, 3))
 
         (hxx,hxy) ||> List.zip
-    
+
     // Cell cluster by specified count
-    let cls (ct : int) oc (x : double , y : double)  =
+    let cls (ct : int) (oc : (int * int)list) (x : int , y : int)  =
         // ct : Number of cells required
         // oc : List of occupied cells (can be none)
 
@@ -40,7 +45,7 @@ module HexShapes =
         cl ct ls oc
 
     // Perimeter cells in cluster
-    let prm (ce : (float*float)list) =
+    let prm (ce : (int*int)list) =
         // ce : All cells in cluster
 
         List.filter (fun x -> 
@@ -49,7 +54,7 @@ module HexShapes =
                 |> List.contains false) ce
 
     // Incremental cells at multiple clusters (start)
-    let mcs (ct : int) (hc : (float*float)list) =  
+    let mcs (ct : int) (hc : (int*int)list) =
         // hc : Host cluster
         // ct : Number of sub clusters
 
@@ -63,8 +68,8 @@ module HexShapes =
         //Multiple clusters with common host 
         let mlc = 
             let ic = 3
-            List.replicate ct (prm hc)
-            |> List.scan (fun x y -> clh ic x y) (prm hc)
+            List.replicate ct hc
+            |> List.scan (fun x y -> clh ic x y) hc
             |> List.map (fun x -> List.distinct x)
         List.map2 (fun x y -> List.except x y) (List.truncate ct mlc) (List.tail mlc)
 
@@ -75,5 +80,5 @@ module HexShapes =
 
     let tupLst tup = List.map (fun x -> tupXY x) tup
 
-    let fd = (cls 1 [] (0.0,0.0) )
-    let cc = mcs 10 fd
+    let fd = (cls 1 [] (0,0) )
+    let cc = mcs 100 fd
