3fa3766195e265909c320e40e5e114136565016c
Vikram Subbaiah <vikramsubbaiah@Vikrams-MacBook-Pro.local>
2020-08-13 12:42:44 +0530

    20200813

---
+++ SpacePlanning/types.fs
@@ -0,0 +1,20 @@
+namespace Typ
+    module b =
+        type Hxl = int * int * int
+
+        //Surrounding Hexels
+        let adjHxl (hxl : Hxl) =
+            let a,b,c = hxl
+            let x = [0; -1; -2; -1; 1; 2; 1] |> List.map (fun u -> u + a)
+            let y = [0; -2; 0; 2; 2; 0; 2] |> List.map (fun v -> v + b)
+            let z = [0; 0; 0; 0; 0; 0; 0] |> List.map (fun w -> w + c)
+            List.zip3 x y z |> List.map Hxl
+
+        //Incremental Hexel
+        let incHxl (hxl : Hxl) (occ : Hxl list) : Hxl = 
+            adjHxl hxl |> List.except occ |> List.head
+
+        //let clsHxl (hxls,cnts : Hxl * int list ) 
+
+        
+        let g = incHxl (0,0,0 : Hxl) [0,0,0;-1,-2,0]
