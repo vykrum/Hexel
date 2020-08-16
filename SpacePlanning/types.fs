cbc1e8b21399ec4facfb0effe4e928fb9e2d46e4
Vikram Subbaiah <vikramsubbaiah@Vikrams-MacBook-Pro.local>
2020-08-16 07:49:07 +0530

    20200816

--- SpacePlanning/types.fs
+++ SpacePlanning/types.fs
@@ -5,16 +5,34 @@ namespace Typ
         //Surrounding Hexels
         let adjHxl (hxl : Hxl) =
             let a,b,c = hxl
-            let x = [0; -1; -2; -1; 1; 2; 1] |> List.map (fun u -> u + a)
-            let y = [0; -2; 0; 2; 2; 0; 2] |> List.map (fun v -> v + b)
-            let z = [0; 0; 0; 0; 0; 0; 0] |> List.map (fun w -> w + c)
+            let x = [-1; -2; -1; 1; 2; 1] 
+                    |> List.map (fun u -> u + a)
+            let y = [-2; 0; 2; 2; 0; -2] 
+                    |> List.map (fun v -> v + b)
+            let z = [0; 0; 0; 0; 0; 0] 
+                    |> List.map (fun w -> w + c)
             List.zip3 x y z |> List.map Hxl
 
+        // Perimeter cells
+        let prmHxl (ce : Hxl list) =
+            // ce : All cells in cluster
+            List.filter (fun x -> 
+                adjHxl x |> List.map (fun x -> 
+                    List.contains x ce) 
+                    |> List.contains false) ce
+
         //Incremental Hexel
-        let incHxl (hxl : Hxl) (occ : Hxl list) : Hxl = 
-            adjHxl hxl |> List.except occ |> List.head
+        let incHxl (hxl : Hxl list) (occ : Hxl list) : Hxl list= 
+            let inc = hxl 
+                    |> prmHxl 
+                    |> List.head 
+                    |> adjHxl 
+                    |> List.except (hxl @ occ) 
+                    |> List.head 
+            List.append hxl [inc] 
 
-        //let clsHxl (hxls,cnts : Hxl * int list ) 
+        //List.fold (fun a b -> a + string(b)) "" [1;2;3;4;5]
 
-        
-        let g = incHxl (0,0,0 : Hxl) [0,0,0;-1,-2,0]
+        //let clsHxl (hxls,cnts : Hxl * int list ) (occ : Hxl list) = 
+
+        //incHxl [-3,-2,0; -4,-4,0; -5,-2,0] [0,0,0; -1,-2,0; -2,0,0; -1,2,0; 1,2,0; 2,0,0; 1,-2,0; -2,-4,0; -3,-2,0]
