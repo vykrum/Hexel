51c557731da2c4fe0c9dea2efc59d0186ee401a3
vykrum <testingwaters@gmail.com>
2023-10-08 18:06:36 +0530

    20231008

--- SpacePlanning/fsx/Parse/xml/Xml20230627.fsx
+++ SpacePlanning/fsx/Parse/xml/Xml20230627.fsx
@@ -1,32 +1,32 @@
 let xmls = """
-<residence>
-    <foyer value="5">
-        <study value="10"></study>
-    </foyer>
-    <living value="20"></living>
-    <dining value="20">
-        <bed-1 value="15">
-            <bath-1 value="5"></bath-1>
-        </bed-1>
-        <bed-2 value="15">
-            <dress-2 value="5">
-                <bath-2 value="5"></bath-2>
-            </dress-2>
-        </bed-2>
-        <bed-3 value="15">
-            <dress-3 value="5"></dress-3>
-            <bath-3 value="5"></bath-3>
-        </bed-3>
-        <kitchen value="15">
-            <utility value="5"></utility>
-        </kitchen>
-    </dining>
-    <staircase value="20"></staircase>
+<residence id="0">                        
+  <foyer id="1" size="5">            
+    <study id="1.1" size="10"></study>                                    
+  </foyer>                           
+  <living id="2" size="20"></living> 
+  <dining id="3" size="20">          
+    <bed-1 id="3.1" size="15">       
+      <bath-1 id="3.1.1" size="5"></bath-1>                               
+    </bed-1>                         
+    <bed-2 id="3.2" size="15">       
+      <dress-2 id="3.2.1" size="5">  
+        <bath-2 id="3.2.1.1" size="5"></bath-2>                           
+      </dress-2>                     
+    </bed-2>                         
+    <bed-3 id="3.3" size="15">       
+      <dress-3 id="3.3.1" size="5"></dress-3>                             
+      <bath-3 id="3.3.2" size="5"></bath-3>                               
+    </bed-3>                         
+    <kitchen id="3.4" size="15">     
+      <utility id="3.4.1" size="5"></utility>                             
+    </kitchen>                       
+  </dining>                          
+  <staircase id="4" size="15"></staircase>                                
 </residence>
 """
 
 // Test
 open System.Xml.Linq
-let xd = XDocument.Load("""C:\Users\vykru\Github\Hexel\SpacePlanning\fsx\Parse\xml\space.xml""")
-let xs = xd.ToString
+let xd = XDocument.Load("""C:\Users\vykru\Github\Hexel\SpacePlanning\fsx\Parse\xml\space1.xml""")
+let xs = xd.Nodes()
 
