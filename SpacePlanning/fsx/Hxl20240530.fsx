b21d338e15403bbfce02ebaf7bff81c23617ec70
vykrum <testingwaters@gmail.com>
2024-06-28 14:51:27 +0530

    20240628

--- SpacePlanning/fsx/Hxl20240530.fsx
+++ SpacePlanning/fsx/Hxl20240530.fsx
@@ -867,7 +867,7 @@ module Parse =
             let chHx = bsCx.Hxls |> Array.filter (fun x -> (AV(hxlCrd x))=x)
             let cnt = (Array.length tre) - 1
             //
-            // Rework to distribute room anchors......
+            // Rework to distribute room anchors.......
             //
             let chBs = match (Array.length chHx) >= cnt with 
                         | true -> Array.take cnt chHx
