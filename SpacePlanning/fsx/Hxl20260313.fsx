262679ddd5ee1a01b0956699bfef5de5f24b2c4b
vykrum <testingwaters@gmail.com>
2026-06-15 20:44:41 +0530

    20260615-1

--- SpacePlanning/fsx/Hxl20260313.fsx
+++ SpacePlanning/fsx/Hxl20260313.fsx
@@ -1565,7 +1565,7 @@ let cxx1 = coxel sq 0 ([|
             AV (3, -2, 0), Refid "3", Count 10, Label "C";
             |]) hx11 
 
-let st = hxlVld sq (AV(0,0,0))
+//let st = hxlVld sq (AV(0,0,0))
 //let en = hxlVld sq (AV(20,0,0))
 
 // Create parsing for levels
